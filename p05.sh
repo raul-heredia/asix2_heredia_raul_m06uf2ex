b78625892f32dbf296d0232bc7d418d62149b434
Raúl Heredia <raul.heredia01@outlook.com>
2021-04-22 17:24:28 +0200

    Finished p05.sh and p12.sh

--- p05.sh
+++ p05.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Aquest script ha de ser executat com a root" 
+   exit 1
+fi
+
+REPETEIX=1
+
+while [[ $REPETEIX -eq 1 ]]; do
+clear
+echo -n "Quin paquet vols instal·lar?: "
+read PAQUET
+
+apt update > /dev/null 2>&1
+
+apt install $PAQUET -y > /dev/null 2>&1
+
+if [[ $? -eq 0 ]]; then
+    echo "El paquet $PAQUET s'ha instal·lat correctament"
+else
+    echo "Comprova la connectivitat i/o el nom del paquet."
+fi
+
+echo -n "Vols continuar? Prem '1' per continuar: "
+read REPETEIX
+done
+exit 20
--- p12.sh
+++ p12.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+REGEX='\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b'
+
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Aquest script ha de ser executat com a root" 
+   exit 1
+fi
+
+echo -n "Indica una adreça IP: "
+read ADDRESS
+
+if [[ $ADDRESS =~ $REGEX ]]
+then    
+	nmap -sS $ADDRESS
+else
+    echo "Adreça incorrecta"
+	exit 10
+fi
+exit 0
