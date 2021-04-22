1ac4cabe94f946900bff3e0fe1f8a629e8b81a62
Raúl Heredia <raul.heredia01@outlook.com>
2021-04-22 17:10:07 +0200

    P09.sh finished

--- p09.sh
+++ p09.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+clear
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Aquest script ha de ser executat com a root" 
+   exit 1
+fi
+
+echo -n "Introdueix el nom d'usuari: "
+read USUARI
+
+id -u ${USUARI} 2>/dev/null
+
+if [[ $? -ne 0 ]]; then
+    echo "[ERROR] L'usuari ${USUARI} no existeix al sistema"
+    exit 1
+fi
+exit 0
